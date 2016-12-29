17a0592866fe2ef3f1af14a13e53c1cbc73b46eb
rajat2797 <rajatsinghal1997@gmail.com>
2016-12-29 12:45:37 +0530

    altbeacon lib

--- app/src/main/java/com/example/rajat/app/MainActivity.java
+++ app/src/main/java/com/example/rajat/app/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.*;
@@ -97,6 +100,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        if(item.getItemId() == R.id.sign_out_menu) {
+            AuthUI.getInstance().signOut(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
 }
